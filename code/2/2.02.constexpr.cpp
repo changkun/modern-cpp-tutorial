5c71bf09165931bbe453273297b1ec0f76caa178
Adem Budak <adembudak@outlook.com>
2019-01-10 01:42:16 +0300

    Add more description

--- code/2/2.02.constexpr.cpp
+++ code/2/2.02.constexpr.cpp
@@ -7,7 +7,6 @@
 //
 
 #include <iostream>
-#define LEN 10
 
 int len_foo() {
     int i = 2;
@@ -17,30 +16,31 @@ constexpr int len_foo_constexpr() {
     return 5;
 }
 
-// error in c++11
-// constexpr int fibonacci(const int n) {
-//     if(n == 1) return 1;
-//     if(n == 2) return 1;
-//     return fibonacci(n-1) + fibonacci(n-2);
-// }
-
-// ok
+// error in C++11 due to more than one return statement, okay C++14 and above
 constexpr int fibonacci(const int n) {
+     if(n == 1) return 1;
+     if(n == 2) return 1;
+     return fibonacci(n-1) + fibonacci(n-2);
+}
+
+// ok in C++11, single return
+constexpr int fibonacci2(const int n) {
     return n == 1 || n == 2 ? 1 : fibonacci(n-1) + fibonacci(n-2);
 }
 
 
 int main() {
+    const int len =10;
     char arr_1[10];                      // legal
-    char arr_2[LEN];                     // legal
+    char arr_2[len];                     // legal
 
-    int len = 10;
-    // char arr_3[len];                  // illegal
+    int len2 = 10;
+    //char arr_3[len2];                  // illegal, container sizes must be constexpr
 
-    const int len_2 = len + 1;
-    constexpr int len_2_constexpr = 1 + 2 + 3;
-    // char arr_4[len_2];                // illegal, but ok for most of the compilers
-    char arr_4[len_2_constexpr];         // legal
+    const int len3 = len + 1;
+    constexpr int len3_constexpr = 1 + 2 + 3;
+   // char arr_4[len3];                // illegal, but ok for most of the compilers
+    char arr_4[len3_constexpr];         // legal
 
     // char arr_5[len_foo()+5];          // illegal
     char arr_6[len_foo_constexpr() + 1]; // legal
