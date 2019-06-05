5c37806f627546b323769339112bb03b2a5cd507
Changkun Ou <hi@changkun.us>
2019-06-05 14:11:55 +0200

    update: more explaination on move proposal

--- code/3/3.4.cpp
+++ code/3/3.4.cpp
@@ -10,22 +10,31 @@
 class A {
 public:
     int *pointer;
-    A():pointer(new int(1)) { std::cout << "构造" << pointer << std::endl; }
-    A(A& a):pointer(new int(*a.pointer)) { std::cout << "拷贝" << pointer << std::endl; }    // 无意义的对象拷贝
-    A(A&& a):pointer(a.pointer) { a.pointer = nullptr;std::cout << "移动" << pointer << std::endl; }
-    ~A(){ std::cout << "析构" << pointer << std::endl; delete pointer; }
+    A():pointer(new int(1)) { 
+        std::cout << "构造" << pointer << std::endl; 
+    }
+    A(A& a):pointer(new int(*a.pointer)) { 
+        std::cout << "拷贝" << pointer << std::endl; 
+    } // 无意义的对象拷贝
+    A(A&& a):pointer(a.pointer) { 
+        a.pointer = nullptr;
+        std::cout << "移动" << pointer << std::endl; 
+    }
+    ~A(){ 
+        std::cout << "析构" << pointer << std::endl; 
+        delete pointer; 
+    }
 };
 // 防止编译器优化
 A return_rvalue(bool test) {
     A a,b;
-    if(test) return a;
-    else return b;
+    if(test) return a; // 等价于 static_cast<A&&>(a);
+    else return b;     // 等价于 static_cast<A&&>(b);
 }
 int main() {
     A obj = return_rvalue(false);
     std::cout << "obj:" << std::endl;
     std::cout << obj.pointer << std::endl;
     std::cout << *obj.pointer << std::endl;
-    
     return 0;
 }
