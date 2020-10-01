e56cc54db248348a3c157f049c8d9b54e36231c3
coderadhika <60996219+coderadhika@users.noreply.github.com>
2020-10-01 13:37:07 +0530

    Update 6.1.regex.cpp

--- code/6/6.1.regex.cpp
+++ code/6/6.1.regex.cpp
@@ -10,26 +10,26 @@
 #include <iostream>
 #include <string>
 #include <regex>
-
+using namespace std;
 int main() {
-    std::string fnames[] = {"foo.txt", "bar.txt", "test", "a0.txt", "AAA.txt"};
+    string fnames[] = {"foo.txt", "bar.txt", "test", "a0.txt", "AAA.txt"};
     // In C++, `\` will be used as an escape character in the string. 
     // In order for `\.` to be passed as a regular expression, 
     // it is necessary to perform second escaping of `\`, thus we have `\\.`
-    std::regex txt_regex("[a-z]+\\.txt");
+    regex txt_regex("[a-z]+\\.txt");
     for (const auto &fname: fnames)
-        std::cout << fname << ": " << std::regex_match(fname, txt_regex) << std::endl;
+        cout << fname << ": " << regex_match(fname, txt_regex) << endl;
     
-    std::regex base_regex("([a-z]+)\\.txt");
-    std::smatch base_match;
+   regex base_regex("([a-z]+)\\.txt");
+    smatch base_match;
     for(const auto &fname: fnames) {
-        if (std::regex_match(fname, base_match, base_regex)) {
+        if (regex_match(fname, base_match, base_regex)) {
             // the first element of std::smatch matches the entire string
             // the second element of std::smatch matches the first expression with brackets
             if (base_match.size() == 2) {
-                std::string base = base_match[1].str();
-                std::cout << "sub-match[0]: " << base_match[0].str() << std::endl;
-                std::cout << fname << " sub-match[1]: " << base << std::endl;
+                string base = base_match[1].str();
+                cout << "sub-match[0]: " << base_match[0].str() << endl;
+                cout << fname << " sub-match[1]: " << base <<endl;
             }
         }
     }
