d86881143c3a1b96b847c35354d0b0974223a4f0
coderadhika <60996219+coderadhika@users.noreply.github.com>
2020-10-01 13:27:45 +0530

    Update 2.11.for.loop.cpp

--- code/2/2.11.for.loop.cpp
+++ code/2/2.11.for.loop.cpp
@@ -10,15 +10,15 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+using namespace std;
 int main() {
-    std::vector<int> vec = {1, 2, 3, 4};
-    if (auto itr = std::find(vec.begin(), vec.end(), 3); itr != vec.end()) *itr = 4;
+    vector<int> vec = {1, 2, 3, 4};
+    if (auto itr = find(vec.begin(), vec.end(), 3); itr != vec.end()) *itr = 4;
     for (auto element : vec)
-        std::cout << element << std::endl; // read only
+        cout << element << endl; // read only
     for (auto &element : vec) {
         element += 1;                      // writeable
     }
     for (auto element : vec)
-        std::cout << element << std::endl; // read only
+        cout << element << endl; // read only
 }
