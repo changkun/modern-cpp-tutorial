57da841b4c5d5ef18ee48a4f285994425de45d1d
Raul123-dev <73396318+Raul123-dev@users.noreply.github.com>
2020-10-24 14:10:14 -0500

    Agregue using namespace std
    para eliminar el std:: de la estructura main(){}

--- exercises/2/fold.expresion.cpp
+++ exercises/2/fold.expresion.cpp
@@ -9,10 +9,11 @@
 //
 
 #include <iostream>
+using namespace std;
 template<typename ... T>
 auto average(T ... t) {
     return (t + ... ) / sizeof...(t);
 }
 int main() {
-    std::cout << average(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) << std::endl;
+    cout << average(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) <<endl;
 }
